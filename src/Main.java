56eb266557473753584be703fe7d0f2dd10746c1
Emma Parisgit config --global user.name Skream <tumho00005@gmail.com>
2023-08-12 19:26:00 +0300

    Change Zip

--- src/Main.java
+++ src/Main.java
@@ -3,62 +3,66 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static String createFolder(String url) {
+        String folder = "";
+        File file = new File(url);
+        if (file.exists()) folder = file.getName();
+        else if (file.mkdirs()) {
+            folder = file.getName();
+        }
+        return folder;
+    }
+
+    public static String createFile(List<String> list) throws IOException {
         StringBuilder builder = new StringBuilder();
+        for (String fileName : list) {
+            File cFile = new File(fileName);
+            if (cFile.exists()) builder.append(cFile.getName()).append(",");
+            else if (cFile.createNewFile()) {
+                builder.append(cFile.getName()).append(",");
+            }
+        }
+        return builder.toString();
+    }
 
-        File directory = new File("/Users/mustafatumgoev/Games");
-        if (directory.exists()) builder.append(directory.getName()).append(",");
-        else if (directory.mkdirs()) builder.append(directory.getName()).append(",");
+    public static void main(String[] args) {
+        String url = "/Users/mustafatumgoev/Games";
 
-        File srcFolder = new File(directory, "src/res/savegame/temp");
-        if (srcFolder.exists()) builder.append("src,res,savegame,").append(srcFolder.getName()).append(",");
-        else if (srcFolder.mkdirs()) builder.append("src,res,savegame,").append(srcFolder.getName()).append(",");
+        StringBuilder builder = new StringBuilder();
+        builder.append(createFolder(url)).append(",");
+        builder.append(createFolder(url + "/src")).append(",");
+        builder.append(createFolder(url + "/src/res")).append(",");
+        builder.append(createFolder(url + "/src/res/savegame")).append(",");
+        builder.append(createFolder(url + "/src/res/savegame/temp")).append(",");
+        builder.append(createFolder(url + "/src/main")).append(",");
+        builder.append(createFolder(url + "/src/test")).append(",");
 
-        File mainFolder = new File(directory, "src/main");
-        if (mainFolder.exists()) builder.append(mainFolder.getName()).append(",");
-        else if (mainFolder.mkdirs()) builder.append(mainFolder.getName()).append(",");
+        builder.append(createFolder(url + "/src/res/drawables")).append(",");
+        builder.append(createFolder(url + "/src/res/vectors")).append(",");
+        builder.append(createFolder(url + "/src/res/icons")).append(",");
 
-        File testFolder = new File(directory, "src/test");
-        if (testFolder.exists()) builder.append(testFolder.getName()).append(",");
-        else if (testFolder.mkdirs()) builder.append(testFolder.getName()).append(",");
+        List<String> list = Arrays.asList(
+                url + "/src/main/Main.java",
+                url + "/src/main/Utils.java",
+                url + "/src/res/savegame/temp/temp.txt");
         try {
-            File file = new File(mainFolder, "Main.java");
-            if (file.exists()) builder.append(file.getName()).append(",");
-            else if (file.createNewFile()) builder.append(file.getName()).append(",");
-
-            File fileTwo = new File(mainFolder, "Utils.java");
-            if (fileTwo.exists()) builder.append(fileTwo.getName()).append(",");
-            else if (fileTwo.createNewFile()) builder.append(fileTwo.getName()).append(",");
+            builder.append(createFile(list)).append(",");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        File drawablesFolder = new File(directory, "src/res/drawables");
-        if (drawablesFolder.exists()) builder.append(drawablesFolder.getName()).append(",");
-        else if (drawablesFolder.mkdirs()) builder.append(drawablesFolder.getName()).append(",");
-
-        File vectorsFolder = new File(directory, "src/res/vectors");
-        if (vectorsFolder.exists()) builder.append(testFolder.getName()).append(",");
-        else if (vectorsFolder.mkdirs()) builder.append(vectorsFolder.getName()).append(",");
-
-        File iconsFolder = new File(directory, "src/res/icons");
-        if (iconsFolder.exists()) builder.append(iconsFolder.getName()).append(",");
-        else if (iconsFolder.mkdirs()) builder.append(iconsFolder.getName()).append(",");
-
-        File tempFile = new File(srcFolder, "temp.txt");
-        try {
-            if (tempFile.exists()) builder.append(tempFile.getName()).append(",");
-            else if (tempFile.createNewFile()) builder.append(tempFile.getName()).append(",");
+        try (FileWriter writer = new FileWriter(url + "/src/res/savegame/temp/temp.txt")) {
+            writer.write(builder.toString());
+            writer.flush();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        try (FileWriter writer = new FileWriter(tempFile)) {
-           writer.write(builder.toString());
-           writer.flush();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println(builder.toString());
     }
 }
